1388f56e2150d3503b1ff01b58a50b60f026c684
abhi989023 <awasthiabhishek703@gmail.com>
2022-04-09 01:47:38 +0530

    Added UC1

---
+++ EmpWageBuilder.java
@@ -0,0 +1,17 @@
+package EmpAttd;
+
+public class EmpWageBuilder {
+	
+	public static void main(String[] args)
+	{
+		double checkAtt=Math.floor(Math.random()*10)%2;
+		if(checkAtt==1)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
+	}
+}
